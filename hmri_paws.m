a7e3944d2563b800003d81c1917346eb6f07023d
BarisUgurcan <beu4@cornell.edu>
2024-12-18 18:19:18 +0100

    outline of function

--- hmri_paws.m
+++ hmri_paws.m
@@ -8,6 +8,14 @@ function [denoised_weighted_data] = hmri_paws(weighted_data, params)
 %%outputs:
 %denoised_weighted_data: adaptive denoised volumes
 
+%call hmri_calc_R2s with 4 arguments (including design matrix D and SError)
+
+%from R2s and extrapolates calculate residuals and variance (using design
+%matrix)
+
+%call PAWS with data, residuals, params (include here?)
+
+%take out denoised_weighted_data
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
