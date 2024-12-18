7ee75941fd744d83b027611f93c63d91cf56b43f
BarisUgurcan <beu4@cornell.edu>
2024-12-18 18:14:06 +0100

    define preliminary input and outputs

--- hmri_paws.m
+++ hmri_paws.m
@@ -1,5 +1,13 @@
-function [outputArg1,outputArg2] = hmri_paws(inputArg1,inputArg2)
+function [denoised_weighted_data] = hmri_paws(weighted_data, params)
 %main function that does PAWS
+
+%%inputs
+%weighted_data (cell): cell array of available contrasts
+%params (str): 3 adaptive denoising parameters kstar, lambda, patchsize
+
+%%outputs:
+%denoised_weighted_data: adaptive denoised volumes
+
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
