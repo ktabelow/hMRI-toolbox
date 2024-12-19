1eddc14c2736c1506b0ce128f5b09f7d461df11f
BarisUgurcan <beu4@cornell.edu>
2024-12-19 10:57:34 +0100

    calc residuals over all contrasts and variance

--- hmri_paws.m
+++ hmri_paws.m
@@ -13,10 +13,23 @@ function [denoised_weighted_data] = hmri_paws(weighted_data, params)
 %call hmri_coreg before denoising
 
 %call hmri_calc_R2s with 4 arguments (including design matrix D and SError)
-[R2s, intercepts, SError, D] = hmri_calc_R2s(dataToFit,fit_method);
+[R2s, extrapolated, SError, D] = hmri_calc_R2s(dataToFit,fit_method);
 
 %from R2s and extrapolates calculate residuals and variance (using design
 %matrix)
+
+res_var = 0;
+num_echo=0;
+for ccon =1:numconn
+    for te = data[ccon].TE
+        res_var = res_var + (data[cccon].te -  extrapolated{ccon}*exp(-R2s*te))^2;
+    end
+    num_echo = num_echo + length(data[ccon].TE);
+end
+res_var = res_var / (num_echo - numconn + 1);
+
+variance = res_var * inv(D'D);
+
 
 
 %call PAWS with data, residuals, params (include here?)
