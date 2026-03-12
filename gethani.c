551aaf6756f7c9ced3859e19cf002da0cec06ce6
Karsten Tabelow <tabelow@mardi4nfdi.de>
2026-03-12 08:59:08 +0100

    first try translating the gethani function

---
+++ gethani.c
@@ -0,0 +1,115 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to compute the location kernel
+static double lkern(int kern, double xsq) {
+    double z;
+    if (xsq >= 1.0) {
+        return 0.0;
+    } else if (kern == 1) {
+        if (xsq <= 0.5) {
+            return 1.0;
+        } else {
+            return 2.0 * (1.0 - xsq);
+        }
+    } else if (kern == 2) {
+        return 1.0 - xsq;
+    } else if (kern == 3) {
+        z = 1.0 - xsq;
+        return z * z;
+    } else if (kern == 4) {
+        z = 1.0 - xsq;
+        return z * z * z;
+    } else if (kern == 5) {
+        return exp(-xsq * 8.0);
+    } else {
+        // Default to Epanechnikov
+        return 1.0 - xsq;
+    }
+}
+
+// Function to compute the sum of location weights
+static double sofw(double bw, int kern, double wght[2]) {
+    double h2 = bw * bw;
+    int ih3 = (int)floor(bw * wght[1]);
+    int ih2 = (int)floor(bw * wght[0]);
+    int ih1 = (int)floor(bw);
+    int dlw1 = 2 * ih1 + 1;
+    int dlw2 = 2 * ih2 + 1;
+    int dlw3 = 2 * ih3 + 1;
+    int clw1 = (dlw1 + 1) / 2;
+    int clw2 = (dlw2 + 1) / 2;
+    int clw3 = (dlw3 + 1) / 2;
+    double sw = 0.0;
+    double sw2 = 0.0;
+    
+    for (int j1 = 1; j1 <= dlw1; j1++) {
+        double z1 = (clw1 - j1);
+        z1 = z1 * z1;
+        if (wght[0] > 0.0) {
+            ih2 = (int)floor(sqrt(h2 - z1) * wght[0]);
+            for (int j2 = clw2 - ih2; j2 <= clw2 + ih2; j2++) {
+                double z2 = (clw2 - j2) / wght[0];
+                z2 = z1 + z2 * z2;
+                if (wght[1] > 0.0) {
+                    ih3 = (int)floor(sqrt(h2 - z2) * wght[1]);
+                    for (int j3 = clw3 - ih3; j3 <= clw3 + ih3; j3++) {
+                        double z3 = (clw3 - j3) / wght[1];
+                        double z = lkern(kern, (z3 * z3 + z2) / h2);
+                        sw += z;
+                        sw2 += z * z;
+                    }
+                } else {
+                    double z = lkern(kern, z2 / h2);
+                    sw += z;
+                    sw2 += z * z;
+                }
+            }
+        } else {
+            double z = lkern(kern, z1 / h2);
+            sw += z;
+            sw2 += z * z;
+        }
+    }
+    return sw * sw / sw2;
+}
+
+// Main function to compute bandwidth
+void cgethani(double x, double y, int kern, double value, double wght[2], double eps, double *bw) {
+    if (x >= y) {
+        *bw = 1.0;
+        return;
+    }
+    double fw1 = sofw(x, kern, wght);
+    double fw2 = sofw(y, kern, wght);
+    
+    while (fw1 > value) {
+        x = x * x / y;
+        fw1 = sofw(x, kern, wght);
+    }
+    
+    while (fw2 <= value) {
+        y = y * y / x;
+        fw2 = sofw(y, kern, wght);
+    }
+    
+    while (fmin(fw2 / value, value / fw1) > 1.0 + eps) {
+        double z = x + (value - fw1) / (fw2 - fw1) * (y - x);
+        double fw3 = sofw(z, kern, wght);
+        if (fw3 <= value) {
+            x = z;
+            fw1 = fw3;
+        }
+        if (fw3 >= value) {
+            y = z;
+            fw2 = fw3;
+        }
+    }
+    
+    if (fw2 / value > value / fw1) {
+        *bw = x + (value - fw1) / (fw2 - fw1) * (y - x);
+    } else {
+        *bw = y - (fw2 - value) / (fw2 - fw1) * (y - x);
+    }
+}
---
+++ gethani_mex.c
@@ -0,0 +1,66 @@
+#include "mex.h"
+#include <math.h>
+
+// Gateway function gethani 
+//  for function cgethani in gethani.c
+//  to be called:
+//  hakt = gethani(x,      # 1
+//                 y,      # 2
+//                 kern,   # 3
+//                 value,  # 4
+//                 wght,   # 5
+//                 eps);   # 6
+//
+
+// MEX function gateway
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+    double x, y, value, eps, *wght, bw;
+    int kern;
+    
+    // Check for proper number of arguments
+    if (nrhs != 6) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidNumInputs", "GETHANI requires 6 input arguments");
+    }
+    if (nlhs != 1) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidNumOutputs", "GETHANI requires 1 output argument");
+    }
+    
+    // Check if inputs are numeric
+    if (!mxIsNumeric(prhs[0])) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidInput", "X has to be a number");
+    }
+    if (!mxIsNumeric(prhs[1])) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidInput", "Y has to be a number");
+    }
+    if (!mxIsNumeric(prhs[2])) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidInput", "KERN has to be a number");
+    }
+    if (!mxIsNumeric(prhs[3])) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidInput", "VALUE has to be a number");
+    }
+    if (!mxIsNumeric(prhs[4])) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidInput", "WGHT has to be a vector");
+    }
+    if (!mxIsNumeric(prhs[5])) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidInput", "EPS has to be a number");
+    }
+    
+    // Get scalar inputs
+    x = mxGetScalar(prhs[0]);
+    y = mxGetScalar(prhs[1]);
+    kern = (int)mxGetScalar(prhs[2]);
+    value = mxGetScalar(prhs[3]);
+    eps = mxGetScalar(prhs[5]);
+    
+    // Get wght vector
+    if (mxGetNumberOfElements(prhs[4]) != 2) {
+        mexErrMsgIdAndTxt("MATLAB:gethani:invalidInput", "WGHT has to be a vector of length 2");
+    }
+    wght = mxGetPr(prhs[4]);
+    
+    // Call the gethani function
+    cgethani(x, y, kern, value, wght, eps, &bw);
+    
+    // Create output
+    plhs[0] = mxCreateDoubleScalar(bw);
+}
